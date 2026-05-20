4723a18528c7cab1a7fe7404bfe08cb77ec3d589
kaustubh Pawar <kpawar1925@gmail.com>
2026-05-20 15:07:38 +0530

    Refactor smoothie order submission process

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import requests 
 from snowflake.snowpark.functions import col
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
 
 st.title('🥤 Customize Your Smoothie! 🥤')
 st.write("""Choose The Fruits You Want In Your Custom Smoothie!""")
@@ -10,11 +9,14 @@ st.write("""Choose The Fruits You Want In Your Custom Smoothie!""")
 name_on_order = st.text_input('Name On Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx=st.connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select( col('FRUIT_NAME'), col('SEARCH_ON'))
 
-# ✅ Added max_selections=5
+my_dataframe = session.table("smoothies.public.fruit_options").select(
+    col('FRUIT_NAME'), 
+    col('SEARCH_ON')
+)
+
 ingredients_list = st.multiselect(
     'Choose up to 5 Ingredients:',
     my_dataframe,
@@ -25,35 +27,29 @@ if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen + ' '
-    st.subheader(fruit_chosen + ' Nutrition Information')
-    
-    # Get the SEARCH_ON value for this fruit
-    search_on_df = session.table("smoothies.public.fruit_options") \
-        .filter(col('FRUIT_NAME') == fruit_chosen) \
-        .select(col('SEARCH_ON'))
-    search_on_val = search_on_df.collect()[0][0]
-    
-    # Use SEARCH_ON value in API call instead of FRUIT_NAME
-    smoothiefroot_response = requests.get(
-        "https://my.smoothiefroot.com/api/fruit/" + search_on_val
-    )
-    sf_df = st.dataframe(
-        data=smoothiefroot_response.json(), 
-        use_container_width=True
-    )
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
         
+        # Get the SEARCH_ON value for this fruit
+        search_on_df = session.table("smoothies.public.fruit_options") \
+            .filter(col('FRUIT_NAME') == fruit_chosen) \
+            .select(col('SEARCH_ON'))
+        search_on_val = search_on_df.collect()[0][0]
+        
+        # Use SEARCH_ON value in API call
+        smoothiefroot_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/" + search_on_val
+        )
+        sf_df = st.dataframe(
+            data=smoothiefroot_response.json(), 
+            use_container_width=True
+        )
+
     my_insert_stmt = """insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS, NAME_ON_ORDER)
     values('""" + ingredients_string + """','""" + name_on_order + """')
     """
-
+    
     time_to_insert = st.button('Submit Order')
-
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-
-
- 
-
-
