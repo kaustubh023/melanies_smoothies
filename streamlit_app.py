0b3852dc8d412f43d011498cf91278cb82e3b0ce
kaustubh Pawar <kpawar1925@gmail.com>
2026-05-20 15:25:26 +0530

    Display dataframe with container width in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,11 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(
     col('FRUIT_NAME'), 
     col('SEARCH_ON')
 )
+st.dataframe(data = my_dataframe, use_container_width = True)
+
+
+
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 Ingredients:',
